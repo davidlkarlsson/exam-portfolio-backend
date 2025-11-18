36aae382753016a1da9c355eb47eaef6930980f8
David Karlsson <davidfkarlsson@proton.me>
2025-11-18 18:06:19 +0100

    feat(security): implement JWT authentication (filter, utils, security config)
    - Added JwtAuthenticationFilter with token extraction and validation
    - Added JwtUtils for signing, parsing and validating JWT tokens
    - Integrated JWT into Spring Security filter chain
    - Updated SecurityFilterChain to use stateless sessions and custom auth rules
    - Added role extraction and user authentication using UserDetailsService

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,9 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.resend:resend-java:3.1.0")
     // https://mvnrepository.com/artifact/io.jsonwebtoken/jjwt-api
-    implementation("io.jsonwebtoken:jjwt-api:0.13.0")
+    implementation ("io.jsonwebtoken:jjwt-api:0.12.6" )
+    implementation ("io.jsonwebtoken:jjwt-impl:0.12.6" )
+    implementation ("io.jsonwebtoken:jjwt-jackson:0.12.6" )
 	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
